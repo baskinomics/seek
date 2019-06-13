25cc601accfed491c551101a303cda3f44282fe0
Sean Baskin <seanbaskin@gmail.com>
2019-06-13 12:42:30 -0400

    Micronaut application init

---
+++ src/main/java/seek/Application.java
@@ -0,0 +1,10 @@
+package seek;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
