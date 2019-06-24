97443394bcedeede199dafe168346373fe469ed1
Sean Baskin <seanbaskin@gmail.com>
2019-06-24 19:30:38 -0400

    Javadoc placeholders

--- src/main/java/seek/Application.java
+++ src/main/java/seek/Application.java
@@ -2,8 +2,15 @@ package seek;
 
 import io.micronaut.runtime.Micronaut;
 
+/**
+ * todo documentation
+ */
 public class Application {
 
+    /**
+     * todo documentation
+     * @param args
+     */
     public static void main(String[] args) {
         Micronaut.run(Application.class);
     }
