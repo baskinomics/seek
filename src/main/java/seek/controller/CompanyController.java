020f24e83e2cb55affe40755405abd35d3f8845c
Sean Baskin <seanbaskin@gmail.com>
2019-06-25 17:10:14 -0400

    Fleshed out company service and controller tiers

--- src/main/java/seek/controller/CompanyController.java
+++ src/main/java/seek/controller/CompanyController.java
@@ -1,22 +1,113 @@
 package seek.controller;
 
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.*;
 import seek.domain.dto.CompanyDto;
+import seek.domain.entity.Company;
 import seek.service.CompanyService;
 
 import javax.inject.Inject;
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
+/**
+ * todo Documentation
+ */
 @Controller(value = "/companies")
 public class CompanyController {
 
+    /**
+     * Eligible bean for our company service.
+     */
     @Inject
     protected CompanyService companyService;
 
+    /**
+     * Returns a collection of {@link CompanyDto} instances.
+     *
+     * @return
+     */
     @Get(value = "/list")
     @SuppressWarnings("unused")
     public List<CompanyDto> list() {
         return companyService.list();
+    }
+
+    /**
+     * Returns a {@link Company} instance matching the given {@code id}.
+     *
+     * @param id
+     * @return
+     */
+    @Get("/{id}")
+    @SuppressWarnings("unused")
+    public Company get(final String id) {
+        return companyService.get(UUID.fromString(id));
+    }
+
+    /**
+     * todo Add validation
+     *
+     * @param company
+     * @return
+     */
+    @Post
+    @SuppressWarnings("unused")
+    public HttpResponse<CompanyDto> save(@Body Company company) {
+        final CompanyDto persisted = companyService.save(company);
+
+        return HttpResponse
+                .created(persisted)
+                .headers(headers -> headers.location(location(company.getId())));
+    }
+
+    /**
+     * todo Documentation
+     *
+     * @param company
+     * @return
+     */
+    @Put
+    @SuppressWarnings("unused")
+    public HttpResponse update(@Body Company company) {
+        final CompanyDto updated = companyService.update(company);
+        return HttpResponse
+                .noContent()
+                .header(HttpHeaders.LOCATION, location(updated.getId()).getPath());
+    }
+
+    /**
+     * todo Documentation
+     *
+     * @param id
+     * @return
+     */
+    @Delete("/{id}")
+    @SuppressWarnings("unused")
+    public HttpResponse delete(final String id) {
+        companyService.delete(UUID.fromString(id));
+        return HttpResponse.noContent();
+    }
+
+    /**
+     * todo Documentation
+     *
+     * @param id
+     * @return
+     */
+    protected URI location(UUID id) {
+        return URI.create("/companies/" + id);
+    }
+
+    /**
+     * todo Documentation
+     * 
+     * @param company
+     * @return
+     */
+    protected URI location(Company company) {
+        return location(company.getId());
     }
 }
--- src/main/java/seek/service/CompanyService.java
+++ src/main/java/seek/service/CompanyService.java
@@ -2,26 +2,29 @@ package seek.service;
 
 import io.micronaut.spring.tx.annotation.Transactional;
 import seek.domain.dto.CompanyDto;
+import seek.domain.entity.Company;
 import seek.repository.CompanyRepository;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
- *
+ * todo Documentation
  */
 @Singleton
 public class CompanyService {
 
     /**
-     *
+     * todo Documentation
      */
     @Inject
     protected CompanyRepository companyRepository;
 
     /**
+     * todo Documentation
      *
      * @return
      */
@@ -30,5 +33,48 @@ public class CompanyService {
         return companyRepository.findAll().stream()
                 .map(company -> new CompanyDto(company))
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * todo Convert this to DTO.
+     *
+     * @param id
+     * @return
+     */
+    @Transactional
+    public Company get(final UUID id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * todo Documentation
+     *
+     * @param company
+     * @return
+     */
+    @Transactional
+    public CompanyDto save(final Company company) {
+        return new CompanyDto(companyRepository.save(company));
+    }
+
+    /**
+     * todo Documentation
+     *
+     * @param company
+     * @return
+     */
+    @Transactional
+    public CompanyDto update(final Company company) {
+        return new CompanyDto(companyRepository.update(company));
+    }
+
+    /**
+     * todo Documentation
+     *
+     * @param id
+     */
+    @Transactional
+    public void delete(final UUID id) {
+        companyRepository.deleteById(id);
     }
 }
